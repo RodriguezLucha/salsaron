7cb4a23badeb99df75ea6039885420026edeec38
Rudy <RodriguezLucha@gmail.com>
2019-07-17 16:04:45 -0700

    Add async unit test for login

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -2,13 +2,21 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {wait} from '@testing-library/dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
-it('Has Salseron as the title', () => {
+it('Has Salsaron title', () => {
   const {getByText} = render(<App />);
   expect(getByText('Salsaron')).toBeInTheDocument();
+});
+
+it('Can perform a login', async() => {
+  const {getByText} = render(<App />);
+  await wait(() => {
+    const LoginButton = getByText('Log In');
+  });
 });
