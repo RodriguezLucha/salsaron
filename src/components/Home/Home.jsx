49a5d3c7b9e28581443567cf4d062383ba0fa77a
Rudy <RodriguezLucha@gmail.com>
2019-07-30 19:44:04 -0700

    Fancy up the buttons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styles from './Home.module.scss';
 import {Link} from 'react-router-dom';
 
+
 class Home extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <h1 className={styles.websiteTitle}>Salsaron</h1>
-        <Link
-          className={styles.loginLink}
-          to="/login">Log In</Link>
-        <Link
-          className={styles.signUpLink}
-          to="/signup">Sign Up</Link>
+        <div className={styles.navLinks}>
+          <Link
+            className={styles.loginLink}
+            to="/login">Log In</Link>
+          <Link
+            className={styles.signUpLink}
+            to="/signup">Sign Up</Link>
+        </div>
+        <div className={styles.dancers}>
+          <h1 className={styles.websiteTitle}>Salsaron</h1>
+        </div>
       </div>
     );
   }
