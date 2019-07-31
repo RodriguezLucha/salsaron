6efcb8258f53f0077478cf3c8eadb821d456f698
Rudy <RodriguezLucha@gmail.com>
2019-07-30 18:32:47 -0700

    Updates

---
+++ precache-manifest.0027a84c1fb6ceeb22e4150f3fa24bb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0124f7aad123460b1c6b302285979532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02e40d6474873f945926",
+    "url": "/static/css/main.cb0cfcea.chunk.css"
+  },
+  {
+    "revision": "8906e48b649e1408a630",
+    "url": "/static/js/2.b74c164f.chunk.js"
+  },
+  {
+    "revision": "02e40d6474873f945926",
+    "url": "/static/js/main.ec84b3ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
