1330fbbddafe39f2b67714f7167d698430012594
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-08-11 17:03:31 -0700

    Add user API

--- backend/salsaron/lib/salsaron/Controller/Example.pm
+++ backend/salsaron/lib/salsaron/Controller/Example.pm
@@ -3,10 +3,13 @@ use Mojo::Base 'Mojolicious::Controller';
 
 # This action will render a template
 sub welcome {
-  my $self = shift;
+    my $self = shift;
 
-  # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+    # Render template "example/welcome.html.ep" with message
+    $self->render(
+        text => 'Welcome to the Mojolicious real-time web framework!' );
+
+  # $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
 }
 
 1;
